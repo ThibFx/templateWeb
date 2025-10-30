9c4216e329a324a1cf513e4043cec9e1c0f917b3
Emmanuel MAURICE <emmanuel.maurice@gmail.com>
2025-10-30 15:50:31 +0100

    credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvavdiees73ejoc8g-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_2g8e_user",
+    "password" : "xRU1TwgbcaD0vVsQCfuVUOAbm9m6CAox",
+    "bdname" :"pollution_2g8e" 
     }
 }
 
